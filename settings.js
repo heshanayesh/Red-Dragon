7dc414d9b907c7692409b8b861b057449ee8589d
heshanayesh <109416502+heshanayesh@users.noreply.github.com>
2022-07-22 17:15:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,7 +20,7 @@ global.APIKeys = {
 //customize settings\\
 
 global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+My Creator is heshan ayesh😎` //Costomize Alive Message (type your message in `` )
 
 global.owner = ['+940778688185'] //Owner number in aive msg
 global.premium = ['+940778688185'] //Owner Number info
